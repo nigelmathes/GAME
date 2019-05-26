78e2d61148460838351c504ff2fddf0e0f43faac
TruckThunders <nigelmathes@gmail.com>
2019-05-26 18:15:30 -0400

    Refactored combat logic, can now alter rules!

--- world/actions_api/combat.py
+++ world/actions_api/combat.py
@@ -2,168 +2,162 @@
 Actions to perform for characters
 
 """
-from random import randint
+from random import choice
 
 import actions_api.combat_effects as combat_effects
 from character.models import PlayerClasses, Abilities, StatusEffects
 
 
-def number_to_name(num):
-    """
-    Convert integer number to move name
+class Combat:
+    """ Class to do one round of combat """
+    def __init__(self, player, target,
+                 player_attack_type,
+                 target_attack_type=choice(["area", "attack", "block", "disrupt", "dodge"]),
+                 enhanced=False):
+        """
+        Take in a player and target object and do a combat round
 
-    :param num: Integer corresponding to move
+        :param player:
+        :param target:
+        :param player_attack_type:
+        :param target_attack_type:
+        :param enhanced:
+        """
+        self.player = player
+        self.target = target
+        self.player_attack_type = player_attack_type
+        self.target_attack_type = target_attack_type
+        self.enhanced = enhanced
 
-    :return: String name of move
+        self.rules = {"area": {"beats": ["disrupt", "dodge"],
+                               "loses": ["attack", "block"]},
+                      "attack": {"beats": ["disrupt", "area"],
+                                 "loses": ["block", "dodge"]},
+                      "block": {"beats": ["area", "attack"],
+                                "loses": ["disrupt", "dodge"]},
+                      "disrupt": {"beats": ["block", "dodge"],
+                                  "loses": ["attack", "area"]},
+                      "dodge": {"beats": ["attack", "block"],
+                                "loses": ["area", "disrupt"]}}
 
-    """
-    if num == 0:
-        result = "area"
-    elif num == 1:
-        result = "attack"
-    elif num == 2:
-        result = "block"
-    elif num == 3:
-        result = "dodge"
-    elif num == 4:
-        result = "disrupt"
-    else:
-        result = "attack"
+    def calculate_winner(self):
+        """
+        Function to calculate the winner based upon SOMETHING
 
-    return result
+        :return: Outcome of combat
+        """
+        options_dict = self.rules[self.player_attack_type]
 
+        for result, options in options_dict.items():
+            if self.target_attack_type in options:
+                if result == "beats":
+                    return "player_wins"
+                # result == "loses"
+                else:
+                    return "target_wins"
 
-def name_to_number(name):
-    """
-    Convert move name to integer number
+        # Return tie if result is not in options_dict
+        return "tie"
 
-    :param name: String name of move
+    def do_combat_round(self):
+        """
+        Function to complete one round of combat
 
-    :return: Integer corresponding to move
+        :return: Updated player, target objects
 
-    """
-    if name == "area":
-        result = 0
-    elif name == "attack":
-        result = 1
-    elif name == "block":
-        result = 2
-    elif name == "dodge":
-        result = 3
-    elif name == "disrupt":
-        result = 4
-    else:
-        result = 1
+        Area (0) beats Disrupt (3) and Dodge (4)
+        Attack (1) beats Disrupt (3) and Area (0)
+        Block (2) beats Attack (1) and Area (0)
+        Disrupt (3) beats Block (2) and Dodge (4)
+        Dodge (4) beats Attack (1) and Block (2)
 
-    return result
+        """
+        player_class = PlayerClasses.objects.get(player_class=self.player.character_class)
+        target_class = PlayerClasses.objects.get(player_class=self.target.character_class)
 
+        # Check status effects and apply them to the rules of the game
+        self.check_and_apply_status()
+        self.check_and_apply_status()
 
-def do_combat_round(player, target, player_attack_type, enhanced=False):
-    """
-    Function to complete one round of combat
+        print(f"Player uses {self.player_attack_type},"
+              f" target uses {self.target_attack_type}")
 
-    :param player: Class of player
-    :param target: Class of target
-    :param player_attack_type: area/attack/block/dodge/disrupt
-    :param enhanced: Boolean to tell if to enhance an attack
+        # Determine the winner
+        outcome = self.calculate_winner()
 
-    :return: Updated player, target objects
+        # Player wins!
+        if outcome == "player_wins":
+            ability = Abilities.objects.get(class_id=player_class, type=self.player_attack_type)
+            self.collect_and_resolve_effects(ability, winner=self.player, loser=self.target, enhanced=self.enhanced)
+            print(f"Player wins! Using {ability} ({self.player_attack_type}) on {self.target}")
 
-    Area (0) beats Disrupt (3) and Dodge (4)
-    Attack (1) beats Disrupt (3) and Area (0)
-    Block (2) beats Attack (1) and Area (0)
-    Disrupt (3) beats Block (2) and Dodge (4)
-    Dodge (4) beats Attack (1) and Block (2)
+        # Target wins
+        elif outcome == "target_wins":
+            ability = Abilities.objects.get(class_id=target_class, type=self.target_attack_type)
+            self.collect_and_resolve_effects(ability, winner=self.target, loser=self.player, enhanced=self.enhanced)
+            print(f"Target wins! Using {ability} ({self.target_attack_type}) on {self.player}")
 
-    """
-    player_class = PlayerClasses.objects.get(player_class=player.character_class)
-    target_class = PlayerClasses.objects.get(player_class=target.character_class)
+        # Player and computer tie (clash)
+        else:
+            # Player's attack
+            ability1 = Abilities.objects.get(class_id=player_class, type=self.player_attack_type)
+            self.collect_and_resolve_effects(ability1, winner=self.player, loser=self.target, enhanced=self.enhanced)
 
-    target_attack = randint(0, 4)
-    target_attack_type = number_to_name(target_attack)
-    player_attack = name_to_number(player_attack_type)
+            # Target's attack
+            ability2 = Abilities.objects.get(class_id=target_class, type=self.target_attack_type)
+            self.collect_and_resolve_effects(ability2, winner=self.target, loser=self.player, enhanced=self.enhanced)
+            print(f"Player and target tie! Using both {ability1} on {self.target} and {ability2} on {self.player}")
 
-    #check_and_apply_status(player)
-    #check_and_apply_status(target)
+        print(f"Player HP after combat round: {self.player.hit_points}")
+        print(f"Target HP after combat round: {self.target.hit_points}\n")
 
-    print(f"Player uses {player_attack_type} ({player_attack}), target uses {target_attack_type} ({target_attack})")
-    # Player wins!
-    if (target_attack + 1) % 5 == player_attack:
-        ability = Abilities.objects.get(class_id=player_class, type=player_attack_type)
-        collect_and_resolve_effects(ability, winner=player, loser=target, enhanced=enhanced)
-        print(f"Player wins! (first if) Using {ability} ({player_attack} on {target}")
+        return self.player, self.target
 
-    # Player wins!
-    elif (target_attack + 2) % 5 == player_attack:
-        ability = Abilities.objects.get(class_id=player_class, type=player_attack_type)
-        collect_and_resolve_effects(ability, winner=player, loser=target, enhanced=enhanced)
-        print(f"Player wins! (second if) Using {ability} on {target}")
+    def check_and_apply_status(self):
+        """
+        Function to check player and target status effects, and apply those effects before combat
 
-    # Player and computer tie (clash)
-    elif target_attack == player_attack:
-        # Player's attack
-        ability1 = Abilities.objects.get(class_id=player_class, type=player_attack_type)
-        collect_and_resolve_effects(ability1, winner=player, loser=target, enhanced=enhanced)
+        :return: Updated rules for combat based upon effects
 
-        # Target's attack
-        ability2 = Abilities.objects.get(class_id=target_class, type=target_attack_type)
-        collect_and_resolve_effects(ability2, winner=target, loser=player, enhanced=enhanced)
-        print(f"Player and target tie! Using both {ability1} on {target} and {ability2} on {player}")
+        """
+        player_statuses = self.player.status_effects
+        target_statuses = self.target.status_effects
 
-    # Target wins!
-    else:
-        ability = Abilities.objects.get(class_id=target_class, type=target_attack_type)
-        print(f"Target wins! Using {ability} on {player}")
-
-        # Call effect functions
-        collect_and_resolve_effects(ability, winner=target, loser=player, enhanced=enhanced)
-
-    print(f"Player HP after combat round: {player.hit_points}")
-    print(f"Target HP after combat round: {target.hit_points}\n")
-    return player, target
-
-
-def check_and_apply_status(character):
-    """
-    Function to check player and target status effects, and apply those effects before combat
-
-    :param character: A character object to check status for
-
-    :return: Updated rules for combat based upon effects
-
-    """
-    statuses = character.status_effects
-
-    for status in statuses:
         pass
 
+        #for status in player_statuses:
+        #    pass
 
-def collect_and_resolve_effects(ability, winner, loser, enhanced=False):
-    """
-    Function to go through the effects tied to a given ability
-    and resolve the cumulative effect of all of them
+        #for status in target_statuses:
+        #    pass
 
-    :param ability: Queryset for the ability being evaluated, of the form:
-                    Abilities.objects.get(class_id=target_class, type=player_attack_type)
-    :param winner: The player using the ability
-    :param loser: The target/enemy
-    :param enhanced: Boolean to tell if to enhance an attack
+    @staticmethod
+    def collect_and_resolve_effects(ability, winner, loser, enhanced=False):
+        """
+        Function to go through the effects tied to a given ability
+        and resolve the cumulative effect of all of them
 
-    :return: Cumulative effect and an update/save call to the database
+        :param ability: Queryset for the ability being evaluated, of the form:
+                        Abilities.objects.get(class_id=target_class, type=player_attack_type)
+        :param winner: The player using the ability
+        :param loser: The target/enemy
+        :param enhanced: Boolean to tell if to enhance an attack
 
-    """
-    translation_dictionary = {'target': loser, 'self': winner}
+        :return: Cumulative effect and an update/save call to the database
 
-    # Call effect functions
-    for effect in ability.ability_effects.values():
-        translation_dictionary[effect['target']] = \
-            getattr(combat_effects, effect['function'])(value=effect['value'],
-                                                        target=translation_dictionary[effect['target']])
+        """
+        translation_dictionary = {'target': loser, 'self': winner}
 
-    # Add enhancement if it exists
-    if enhanced:
-        for enhancement in ability.ability_enhancements.values():
-            translation_dictionary[enhancement['target']] = \
-                getattr(combat_effects, enhancement['function'])(value=enhancement['value'],
-                                                                 target=translation_dictionary[enhancement['target']])
-    return winner, loser
+        # Call effect functions
+        for effect in ability.ability_effects.values():
+            translation_dictionary[effect['target']] = \
+                getattr(combat_effects, effect['function'])(value=effect['value'],
+                                                            target=translation_dictionary[effect['target']])
+
+        # Add enhancement if it exists
+        if enhanced:
+            for enhancement in ability.ability_enhancements.values():
+                translation_dictionary[enhancement['target']] = \
+                    getattr(combat_effects, enhancement['function'])(value=enhancement['value'],
+                                                                     target=translation_dictionary[enhancement['target']])
+        return winner, loser
--- world/character/tests.py
+++ world/character/tests.py
@@ -1,81 +1,81 @@
-import mock
 import os
 from django.test import TestCase
-from actions_api.combat import do_combat_round
+from actions_api.combat import Combat
 from character.models import Character, StatusEffects
 
 
 class CombatTests(TestCase):
     fixtures = [os.path.join("/Users", "Nigel", "HOBBY_PROJECTS", "GAME", "world", "full_backup.json")]
 
-    @mock.patch('actions_api.combat.randint')
-    def test_combat_round(self, mocked_randint):
+    def test_combat_round(self):
         """
         A round of combat is done successfully
+
         Player wins with:
             Attack (1) beats Area (0)
-
         """
         # Arrange
         player = Character.objects.get(pk=1)
         target = Character.objects.get(pk=2)
-        attack = 'attack'
         expected_player_hp = 100
         expected_target_hp = 0
 
-        mocked_randint.return_value = 0
+        object_to_test = Combat(player=player,
+                                target=target,
+                                player_attack_type="attack",
+                                target_attack_type="area")
 
         # Act
         # Perform a round of combat
-        player, target = do_combat_round(player=player,
-                                         target=target,
-                                         player_attack_type=attack)
+        player, target = object_to_test.do_combat_round()
 
         # Assert
         self.assertEqual(player.hit_points, expected_player_hp)
         self.assertEqual(target.hit_points, expected_target_hp)
 
-    @mock.patch('actions_api.combat.randint')
-    def test_matchups(self, mocked_randint):
+    def test_matchups(self):
         """
         Test that the following holds true:
 
-        Area (0) beats Disrupt (4) and Dodge (3)
-        Attack (1) beats Disrupt (4) and Area (0)
-        Block (2) beats Attack (1) and Area (0)
-        Dodge (3) beats Attack (1) and Block (2)
-        Disrupt (4) beats Block (2) and Dodge (3)
-
+        Area beats Disrupt and Dodge
+        Attack beats Disrupt and Area
+        Block beats Attack and Area
+        Disrupt beats Block and Dodge
+        Dodge beats Attack and Block
         """
         # Arrange
-        attacks = ['area', 'attack', 'block', 'disrupt', 'dodge']
+        player_attacks = ['area', 'attack', 'block', 'disrupt', 'dodge']
+        target_attacks = ['area', 'attack', 'block', 'disrupt', 'dodge']
         expected_player_hps = [[0, 0, 0, 100, 100],
-                               [100, 0, 0, 0, 100],
+                               [100, 0, 0, 100, 0],
                                [100, 100, 0, 0, 0],
-                               [0, 0, 100, 100, 0],
+                               [0, 0, 100, 0, 100],
                                [0, 100, 100, 0, 0]]
         expected_target_hps = [[0, 100, 100, 0, 0],
-                               [0, 0, 100, 100, 0],
+                               [0, 0, 100, 0, 100],
                                [0, 0, 0, 100, 100],
                                [100, 100, 0, 0, 0],
-                               [100, 0, 0, 0, 100]]
+                               [100, 0, 0, 100, 0]]
 
         # Act
         # Perform a round of combat
-        for i, attack in enumerate(attacks):
-            for j, target_attack_number in enumerate(range(0, 5)):
-                mocked_randint.return_value = target_attack_number
+        for i, player_attack in enumerate(player_attacks):
+            for j, target_attack in enumerate(target_attacks):
                 player = Character.objects.get(pk=1)
                 target = Character.objects.get(pk=2)
-                player, target = do_combat_round(player=player,
-                                                 target=target,
-                                                 player_attack_type=attack)
+                object_to_test = Combat(player=player,
+                                        target=target,
+                                        player_attack_type=player_attack,
+                                        target_attack_type=target_attack)
+
+                player, target = object_to_test.do_combat_round()
+
                 # Assert
                 self.assertEqual(player.hit_points, expected_player_hps[i][j])
                 self.assertEqual(target.hit_points, expected_target_hps[i][j])
 
-    @mock.patch('actions_api.combat.randint')
-    def test_healing(self, mocked_randint):
+    # TODO: Make a healing ability and alter this test
+    def test_healing(self):
         """
         A round of combat is done successfully
 
@@ -83,24 +83,23 @@ class CombatTests(TestCase):
         # Arrange
         player = Character.objects.get(pk=1)
         target = Character.objects.get(pk=2)
-        attack = 'attack'
         expected_player_hp = 0
         expected_target_hp = 100
 
-        mocked_randint.return_value = 2
+        object_to_test = Combat(player=player,
+                                target=target,
+                                player_attack_type="disrupt",
+                                target_attack_type="area")
 
         # Act
         # Perform a round of combat
-        player, target = do_combat_round(player=player,
-                                         target=target,
-                                         player_attack_type=attack)
+        player, target = object_to_test.do_combat_round()
 
         # Assert
         self.assertEqual(player.hit_points, expected_player_hp)
         self.assertEqual(target.hit_points, expected_target_hp)
 
-    @mock.patch('actions_api.combat.randint')
-    def test_enhancement(self, mocked_randint):
+    def test_enhancement(self):
         """
         A round of combat is done successfully
 
@@ -108,19 +107,21 @@ class CombatTests(TestCase):
         # Arrange
         player = Character.objects.get(pk=1)
         target = Character.objects.get(pk=2)
-        attack = 'disrupt'
         expected_player_hp = 100
         expected_target_hp = 0
         expected_status_list = [(1, 2, 'prone', 1)]
 
-        mocked_randint.return_value = 2  # Block
+        # Act
+        # Perform a round of combat
+        object_to_test = Combat(player=player,
+                                target=target,
+                                player_attack_type="disrupt",
+                                target_attack_type="block",
+                                enhanced=True)
 
         # Act
         # Perform a round of combat
-        player, target = do_combat_round(player=player,
-                                         target=target,
-                                         player_attack_type=attack,
-                                         enhanced=True)
+        player, target = object_to_test.do_combat_round()
         status_effects_list = list(StatusEffects.objects.all().values_list())
 
         # Assert
